c9eb1e71b266a91fd5496765381221e919b84432
Ilimmm <whtewn6@gmail.com>
2022-05-24 20:26:19 +0600

    add form axios

--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,44 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 
-const ProductsList = () => {
+const ProductsList = ({getProducts,products, deleteProduct}) => {
   const [selectedProduct, setSelectedProduct] = useState("")
   const [hover, setHover] = useState("")
-    let products = [
-    {
-      title:
-        "Speedracer multifunction watch with Ferrari FXX K 1:43 scale model",
-      image:
-        "https://store.ferrari.com/product_image/560971904581424/F/w640.jpg",
-      price: "175",
-      id: 1,
-    },
-    {
-      title: "Matt black Aspire chronograph watch with steel strap",
-      image:
-        "https://store.ferrari.com/product_image/31840166392133301/F/w640.jpg",
-      price: "295",
-      id: 2,
-    },
-    {
-      title: "Matt grey Aspire chronograph watch with steel strap",
-      image:
-        "https://store.ferrari.com/product_image/31840166392179447/F/w640.jpg",
-      price: "150",
-      id: 3,
-    },
-    {
-      title: "Matt black Aspire chronograph watch with rose gold details",
-      image:
-        "https://store.ferrari.com/product_image/31840166392384740/F/w640.jpg",
-      price: "200",
-      id: 4,
-    },
-  ];
-  console.log(hover)
+    
+  useEffect(()=> {
+    getProducts()
+  }, [])
+  // console.log(deleteProduct)
     return (
         <div className="container d-flex justify-content-between">
-            {products.map((item)=> (<Card onClick={()=> setSelectedProduct(item.id)} onMouseEnter = {()=> setHover(item.id)} onMouseLeave = {()=> setHover ("")} style={{ width: '18rem', border: selectedProduct === item.id ? "1px solid black" : "", backgroundColor: hover === item.id ? "grey" : "white" }}>
+            {products.map((item)=> (<Card
+            key={item.id}
+            onClick={()=> setSelectedProduct(item.id)} onMouseEnter = {()=> setHover(item.id)} onMouseLeave = {()=> setHover ("")} style={{ width: '18rem', border: selectedProduct === item.id ? "1px solid black" : "", backgroundColor: hover === item.id ? "grey" : "white" }}>
   <Card.Img variant="top" src={item.image} />
   <Card.Body>
     <Card.Title>{item.title}</Card.Title>
@@ -46,7 +21,8 @@ const ProductsList = () => {
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Bye</Button>
+    <Button onClick={()=> deleteProduct(item.id)} variant="primary">Delete</Button>
   </Card.Body>
 </Card>))}
         </div>
